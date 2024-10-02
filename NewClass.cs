4b91d6b7c363930a7ffdc0496a61cfd50535003b
Bruno Henrique dos Santos Silva <brunoricksp@gmail.com>
2024-10-01 22:03:24 -0300

    Add files via upload

---
+++ NewClass.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAluno
+{
+    public class Aluno
+    {
+        string? nome = Console.ReadLine();
+        int idade = Convert.ToInt32(Console.ReadLine());
+        long telefone = Convert.ToInt64(Console.ReadLine());
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+/*
+Console.WriteLine("Digite a nota do aluno...");
+int nota = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a frequência do aluno no ano...");
+int freq = Convert.ToInt32(Console.ReadLine());
+
+if (nota >= 6 && freq >= 75)
+
+{
+    Console.WriteLine("O aluno foi aprovado, e passou de ano!");
+}
+
+else if (nota >= 6 && freq <= 74)
+{
+    Console.WriteLine("O aluno teve boa média, mas não está apto por excesso de faltas não justificadas");
+}
+else if (nota <= 6 && freq >= 74)
+{
+    Console.WriteLine("Aluno não atingiu a média necessária para ser aprovado, mesmo frequentando as aulas");
+}
+
+else
+
+{
+    Console.WriteLine("Aluno não atingiu nenhum dos requisitos. Necessita cursar a série novamente");
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bool choveu = true;
+bool estaTarde = true;
+
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou sair para pedalar");
+}
+
+else
+{
+    Console.WriteLine("Vou pedalar outro dia");
+}
